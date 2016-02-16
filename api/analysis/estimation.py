3af37b241a5e8634aee57e804c86f61ae4730a17
dostumut <umit.dost@aalto.fi>
2016-02-16 18:15:16 +0200

    #12 Estimation function logic is applied for django

---
+++ api/analysis/estimation.py
@@ -0,0 +1,28 @@
+import datetime
+
+from ..models import Feedback
+
+
+def calc_fixing_time(service_code):
+    requests = Feedback.query.filter(Feedback.service_code == service_code, Feedback.status == 'closed').all()
+
+    diffs = []
+    for request in requests:
+
+        if request.service_code == service_code:
+            a = request.requested_datetime
+            b = request.updated_datetime
+
+            diff = b - a
+            diffs.append(diff)
+
+    if len(diffs) == 0:
+        return 0
+
+    total_time = sum(diffs, datetime.timedelta())
+    average_time = total_time / len(diffs)
+    return timedelta_milliseconds(average_time)
+
+
+def timedelta_milliseconds(td):
+    return int(td.days * 86400000 + td.seconds * 1000 + td.microseconds / 1000)
