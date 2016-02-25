f04ef660ff0f8026403d8d89ac25b05e5d206576
Ossi Ikonen <ossi.ikonen@aalto.fi>
2016-02-25 18:52:19 +0200

    added filtering fields to the feedback list template

--- frontend/views.py
+++ frontend/views.py
@@ -44,6 +44,7 @@ def feedback_list(request):
     feedbacks = Feedback.objects.all().order_by("-requested_datetime")
     page = request.GET.get("page")
     feedbacks = paginate_query_set(feedbacks, 20, page)
+    servicename = Feedback.objects.values_list('service_name', flat=True).distinct() 
     return render(request, "feedback_list.html", {"feedbacks": feedbacks})
 
 
