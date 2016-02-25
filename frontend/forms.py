9d80e0a7a30c5acffa79332fd4fe3f2393c006fc
Severi Salminen <severi.salminen@iki.fi>
2016-02-25 17:30:07 +0200

    Added cute dog page to form

--- frontend/forms.py
+++ frontend/forms.py
@@ -13,4 +13,4 @@ class FeedbackForm2(forms.Form):
 class FeedbackForm3(forms.Form):
 	title = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Anna otsikko..."}))
 	description = forms.CharField(required=True, widget=forms.Textarea(attrs={'rows': 8, 'maxlength': 5000, 'class': 'form-control', 'placeholder': "Kirjoita tarkka kuvaus..."}))
-	image = forms.FileField()
+	image = forms.FileField(required=False, allow_empty_file=True)
