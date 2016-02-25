033ad4be0a5f894fc46ce7ec6d76f948af3f39f6
Jari Nybäck <jari.nyback@aalto.fi>
2016-02-25 13:35:30 +0200

    Removed map popover and modified to fit mobile screen more better.

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, render_to_response
+import json
+import urllib.request
+
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, render_to_response
 from formtools.wizard.views import SessionWizardView
@@ -6,30 +8,29 @@ from formtools.wizard.views import SessionWizardView
 from api.models import Feedback
 from frontend.forms import FeedbackForm1, FeedbackForm2, FeedbackForm3
 
-import urllib.request, json
-
-
 FORMS = [("location", FeedbackForm1), ("category", FeedbackForm2), ("basic_info", FeedbackForm3)]
 TEMPLATES = {"location": "feedback_form/step1.html", "category": "feedback_form/step2.html",
              "basic_info": "feedback_form/step3.html"}
 
 
 def mainpage(request):
-	context = {}
-	fixed_feedbacks = Feedback.objects.filter(status="closed")[0:4]
-	recent_feedbacks = Feedback.objects.filter(status="open")[0:4]
-	context["fixed_feedbacks"] = fixed_feedbacks
-	context["recent_feedbacks"]  = recent_feedbacks
-	return render(request, "mainpage.html", context)
+    context = {}
+    fixed_feedbacks = Feedback.objects.filter(status="closed")[0:4]
+    recent_feedbacks = Feedback.objects.filter(status="open")[0:4]
+    context["fixed_feedbacks"] = fixed_feedbacks
+    context["recent_feedbacks"] = recent_feedbacks
+    return render(request, "mainpage.html", context)
+
 
 def locations_demo(request):
     feedbacks = Feedback.objects.all()
     return render(request, 'locations_demo.html', {'feedbacks': feedbacks})
 
+
 def feedback_list(request):
     feedbacks = Feedback.objects.all()
     page = request.GET.get("page")
-    feedbacks = paginate_query_set(feedbacks, 20, page)
+    feedbacks = paginate_query_set(feedbacks, 10, page)
     return render(request, "feedback_list.html", {"feedbacks": feedbacks})
 
 
@@ -44,6 +45,7 @@ def paginate_query_set(query_set, items_per_page, page):
         paginate_set = paginator.page(paginator.num_pages)
     return paginate_set
 
+
 def map(request):
     feedbacks = Feedback.objects.all()
     return render(request, "map.html", {"feedbacks": feedbacks})
@@ -51,36 +53,40 @@ def map(request):
 
 # Returns all Helsinki services as a python object
 def get_services():
-	url = "https://asiointi.hel.fi/palautews/rest/v1/services.json?locale=fi_FI"
-	response = urllib.request.urlopen(url)
-	data = json.loads(response.read().decode("utf8"))
-	return data
+    url = "https://asiointi.hel.fi/palautews/rest/v1/services.json?locale=fi_FI"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read().decode("utf8"))
+    return data
+
 
 class FeedbackWizard(SessionWizardView):
     def get_template_names(self):
         return [TEMPLATES[self.steps.current]]
 
-	def get_context_data(self, form, **kwargs):
-		context = super(FeedbackWizard, self).get_context_data(form=form, **kwargs)
-		if self.steps.current == 'category':
-			categories = []
-			data = get_services()
+    def get_context_data(self, form, **kwargs):
+        context = super(FeedbackWizard, self).get_context_data(form=form, **kwargs)
+        if self.steps.current == 'category':
+            categories = []
+            data = get_services()
 
-			GLYPHICONS = ["glyphicon-wrench", "glyphicon-road", "glyphicon-euro", "glyphicon-music", "glyphicon-glass", "glyphicon-heart", "glyphicon-star", "glyphicon-user", "glyphicon-film", "glyphicon-home"]
+            GLYPHICONS = ["glyphicon-wrench", "glyphicon-road", "glyphicon-euro", "glyphicon-music",
+                          "glyphicon-glass", "glyphicon-heart", "glyphicon-star", "glyphicon-user",
+                          "glyphicon-film", "glyphicon-home"]
 
-			for idx, item in enumerate(data):
-				category = {}
-				category["name"] = item["service_name"]
-				category["description"] = item["description"]
-				category["src"] = "https://placehold.it/150x150"
-				category["alt"] = "Category image"
-				category["glyphicon"] = GLYPHICONS[idx]
-				# category["code"] = item["service_code"]
-				categories.append(category)
+            for idx, item in enumerate(data):
+                category = {}
+                category["name"] = item["service_name"]
+                category["description"] = item["description"]
+                category["src"] = "https://placehold.it/150x150"
+                category["alt"] = "Category image"
+                category["glyphicon"] = GLYPHICONS[idx]
+                # category["code"] = item["service_code"]
+                categories.append(category)
 
-			context.update({'categories': categories})
+            context.update({'categories': categories})
 
-		return context
+        return context
 
-	def done(self, form_list, **kwargs):
-		return render_to_response('feedback_form/done.html', {'form_data': [form.cleaned_data for form in form_list]})
+    def done(self, form_list, **kwargs):
+        return render_to_response('feedback_form/done.html',
+                                  {'form_data': [form.cleaned_data for form in form_list]})
