ce6576f640053ac996acd460141a5bf4d10f52f5
Severi Salminen <severi.salminen@iki.fi>
2016-02-28 23:32:28 +0200

    Separated feedback voting code.

---
+++ frontend/static/enable_voting.js
@@ -0,0 +1,26 @@
+// Voting. Do a POST request to record the vote.
+// Voting is attached to feedback_list_vote_icon element.
+function enable_voting(csrf_token_string, voting_url_string) {
+    $(".feedback_list_vote_icon").click(function () {
+        var id = $(this).attr("id");
+        var data = {id: id, csrfmiddlewaretoken: csrf_token_string};
+        $.post(voting_url_string, data, function (data) {
+            if (data.status === "success") {
+                var value = parseInt($(this).next().text()) + 1;
+                $(this).next().text(value);
+                $.notify({
+                    message: data.message
+                }, {
+                    type: "success"
+                });
+            }
+            else {
+                $.notify({
+                    message: data.message
+                }, {
+                    type: "danger"
+                });
+            }
+        }.bind(this));
+    });
+}
---
+++ frontend/static/notify_defaults.js
@@ -0,0 +1,13 @@
+// Set global defaults for Bootstrap-notify plugin.
+
+$.notifyDefaults({
+    type: "success",
+    animate: {
+        enter: 'animated bounceIn',
+        exit: 'animated bounceOut'
+    },
+    placement: {
+        from: "bottom",
+        align: "right"
+    }
+});
