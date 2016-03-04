4d65fa3380d643ace51f01f8994c9609ebbcae65
Severi Salminen <severi.salminen@iki.fi>
2016-03-05 01:25:42 +0200

    Removed file upload. Will replace with JS.

--- frontend/views.py
+++ frontend/views.py
@@ -238,23 +238,15 @@ class FeedbackWizard(SessionWizardView):
         data["location"] = GEOSGeometry('SRID=4326;POINT(' + str(longitude) + ' ' + str(latitude) + ')')
         data["address_string"] = reverse_geocode(latitude, longitude)
 
-        print(form_dict["basic_info"].cleaned_data["attachments"])
-        for file in form_dict["basic_info"].cleaned_data["attachments"]:
-            print("File found!")
-            handle_uploaded_file(file)
-            data["media_url"] = "/media/" + file.name
-
         data["first_name"] = form_dict["contact"].cleaned_data["first_name"]
         data["last_name"] = form_dict["contact"].cleaned_data["last_name"]
         data["email"] = form_dict["contact"].cleaned_data["email"]
         data["phone"] = form_dict["contact"].cleaned_data["phone"]
 
         new_feedback = Feedback(**data)
-        #new_feedback.save()
 
         fixing_time = calc_fixing_time(data["service_code"])
-        expected_datetime = new_feedback.requested_datetime + timedelta(milliseconds=fixing_time)
-        new_feedback.expected_datetime = expected_datetime
+        new_feedback.expected_datetime = new_feedback.requested_datetime + timedelta(milliseconds=fixing_time)
         new_feedback.save()
 
         waiting_time = fixing_time/1000/3600/24
