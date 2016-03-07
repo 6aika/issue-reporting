890a26f68cff3906fb0c9a68f07463b68383c2ad
Severi Salminen <severi.salminen@iki.fi>
2016-03-08 00:08:21 +0200

    Added tooltip for showing estimate in done.html. Added links to return to mainpage or feedback form.

--- api/management/commands/purge_mediafiles.py
+++ api/management/commands/purge_mediafiles.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.utils import timezone
 from api.models import MediaFile
 from datetime import datetime, timedelta
