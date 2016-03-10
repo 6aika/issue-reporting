605cf600c47cfc2cb4d484ada37a69e8afca0dea
Vitaly Ivanov <vitalyivanov88@gmail.com>
2016-03-10 17:41:30 +0200

    fix urls names

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    url(r'^requests/$', views.FeedbackList.as_view()),
-    url(r'^requests/(?P<service_request_id>\w+)/$', views.FeedbackDetail.as_view()),
-    url(r'^services/$', views.ServiceList.as_view()),
+    url(r'^requests/$', views.FeedbackList.as_view(), name='feedback-list'),
+    url(r'^requests/(?P<service_request_id>\w+)/$', views.FeedbackDetail.as_view(), name='feedback-details'),
+    url(r'^services/$', views.ServiceList.as_view(), name='service-list'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
