4f4dd1f9aafc3a322069685ec470c7d21f22b83d
obscurage <samuli.hyttinen@gmail.com>
2016-03-31 17:24:44 +0300

    #127: Prevented choosing start date after end date.

--- frontend/static/calendar.js
+++ frontend/static/calendar.js
@@ -1,6 +1,7 @@
 $(function () {
 	$('#datepicker-start').datetimepicker({
 		defaultDate: moment().subtract(1, 'months'),
+		maxDate: moment(),
 		locale: 'fi',
 		format: 'L',
 	});
@@ -9,6 +10,7 @@ $(function () {
 $(function () {
 	$('#datepicker-end').datetimepicker({
 		defaultDate: moment(),
+		maxDate: moment(),
 		locale: 'fi',
 		format: 'L',
 		showTodayButton: true,
@@ -16,10 +18,11 @@ $(function () {
 });
 
 $(document).ready(function() {
-		$("#datepicker-start").on("dp.change", function (e) {
-            $('#datepicker-end').data("DateTimePicker").minDate(e.date);
-        });
-        $("#datepicker-end").on("dp.change", function (e) {
-            $('#datepicker-start').data("DateTimePicker").maxDate(e.date);
-        });
+	$("#datepicker-start").on("dp.change", function (e) {
+        $('#datepicker-end').data("DateTimePicker").minDate(e.date);
+    });
+    
+    $("#datepicker-end").on("dp.change", function (e) {
+        $('#datepicker-start').data("DateTimePicker").maxDate(e.date);
+    });
 });
