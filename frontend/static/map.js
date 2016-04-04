bf5d1f94f6a749764836336d9eb4191d1f6ccb8a
Severi Salminen <severi.salminen@iki.fi>
2016-04-04 20:21:32 +0300

    Added title to the map legend

--- frontend/static/map.js
+++ frontend/static/map.js
@@ -158,6 +158,7 @@ var legend = L.control({position: "bottomright"});
 
 legend.onAdd = function (map) {
     var div = L.DomUtil.create("div", "map-legend");
+    div.innerHTML += "<h5 class='text-center'>Selite</h5>"
     div.innerHTML += "<i style='background: #D4251C'></i>" + "Avoin palaute<br>";
     div.innerHTML += "<i style='background: #16A427'></i>" + "Suljettu palaute<br>";
     div.innerHTML += "<i style='background: #0072C6'></i>" + "Sijaintisi<br>";
