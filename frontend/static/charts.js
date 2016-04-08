182fc18485dfb5667a1d93bbf90776b4b28fef8f
Vitaly Ivanov <vitalyivanov88@gmail.com>
2016-04-08 18:13:50 +0300

    Move chart spinner logic to charts.js. Fix decimal numbers on Y-axis.

--- frontend/static/charts.js
+++ frontend/static/charts.js
@@ -1,7 +1,37 @@
+var opts = {
+    lines: 11 // The number of lines to draw
+    , length: 0 // The length of each line
+    , width: 14 // The line thickness
+    , radius: 33 // The radius of the inner circle
+    , scale: 1 // Scales overall size of the spinner
+    , corners: 1 // Corner roundness (0..1)
+    , color: '#000' // #rgb or #rrggbb or array of colors
+    , opacity: 0.25 // Opacity of the lines
+    , rotate: 0 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 1.2 // Rounds per second
+    , trail: 60 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '50%' // Top position relative to parent
+    , left: '50%' // Left position relative to parent
+    , shadow: false // Whether to render a shadow
+    , hwaccel: false // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
+};
+
 function humanize(value) {
     return Math.floor(value) + 'pv ' + Math.floor((value * 86400 % 86400) / 3600) + 'h';
 };
 
+var serviceStatisticsDiv = document.getElementById('serviceStatistics');
+var agencyStatisticsDiv = document.getElementById('agencyStatistics');
+var timeStatisticsDiv = document.getElementById('timeStatistics');
+
+var serviceStatisticsSpinner = new Spinner(opts).spin(serviceStatisticsDiv);
+var agencyStatisticsSpinner = new Spinner(opts).spin(agencyStatisticsDiv);
+var timeStatisticsSpinner = new Spinner(opts).spin(timeStatisticsDiv);
 
 $.getJSON("/api/v1/statistics/services/", function (data) {
     var services = ['x'];
@@ -27,6 +57,11 @@ $.getJSON("/api/v1/statistics/services/", function (data) {
             type: 'bar',
         },
         axis: {
+            y: {
+                tick: {
+                    format: d3.format("d")
+                }
+            },
             x: {
                 type: 'category', // this needed to load string x value
                 tick: {
@@ -37,6 +72,8 @@ $.getJSON("/api/v1/statistics/services/", function (data) {
             }
         }
     });
+
+    serviceStatisticsSpinner.stop();
 
     c3.generate({
         bindto: '#timeStatistics',
@@ -71,6 +108,8 @@ $.getJSON("/api/v1/statistics/services/", function (data) {
             }
         }
     });
+
+    timeStatisticsSpinner.stop();
 });
 $.getJSON("/api/v1/statistics/agencies/", function (data) {
     var agencies = ['x'];
@@ -88,6 +127,11 @@ $.getJSON("/api/v1/statistics/agencies/", function (data) {
             right: 12,
         },
         data: {
+            y: {
+                tick: {
+                    format: d3.format("d")
+                }
+            },
             x: 'x',
             columns: [
                 agencies,
@@ -111,4 +155,6 @@ $.getJSON("/api/v1/statistics/agencies/", function (data) {
             }
         }
     });
+
+    agencyStatisticsSpinner.stop();
 });
