45c73a6b7405d865789ab28b6d047bb5eb1fa3d2
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-05-17 20:01:51 +0300

    Improve XML generation support

--- issues/api/transforms.py
+++ issues/api/transforms.py
@@ -5,7 +5,7 @@ from lxml import etree
 from six import text_type
 
 
-def spark_node(node):
+def spark_node(node, in_list=False):
     """
     "Sparkify" a single XML node.
 
@@ -14,7 +14,28 @@ def spark_node(node):
     """
     kids = node.getchildren()
     if kids and all(c.tag == kids[0].tag for c in kids):  # Homogeneous children; this must be an array
-        return [spark_node(k) for k in kids]
+        children = [
+            node for node in [spark_node(k, in_list=True) for k in kids]
+            if node is not None
+        ]
+
+        # The following has not really been specified, but it prevents some mistransforms:
+        if not children:
+            return {}
+
+        if all(isinstance(j_kid, text_type) for j_kid in children):  # All of the children turned into bare strings:
+            return {node.tag: children}  # Wrap them in an object
+        elif all(isinstance(j_kid, dict) and len(j_kid) == 1 for j_kid in children):  # The children got turned into 1-dicts?
+            n_kids = {}
+            for kid in children:
+                n_kids.update(kid)
+            return n_kids  # Unravel them.
+        else:  # Mixed bag? Return the list.
+            return children
+
+    if in_list and not kids and not node.items():
+        return node.text
+
     dct = OrderedDict()
     dct.xml_tag = node.tag
 
--- issues/api/utils.py
+++ issues/api/utils.py
@@ -1,9 +1,7 @@
-from rest_framework.views import exception_handler
-
-
 def api_exception_handler(exc, context):
     # Call REST framework's default exception handler first,
     # to get the standard error response.
+    from rest_framework.views import exception_handler
     response = exception_handler(exc, context)
 
     # Now add the HTTP status code to the response.
@@ -19,6 +17,14 @@ def api_exception_handler(exc, context):
 
 
 class XMLDict(dict):
+
     def __init__(self, dict, xml_tag=None):
         super(XMLDict, self).__init__(dict)
+        self.xml_tag = xml_tag
+
+
+class XMLList(list):
+
+    def __init__(self, list, xml_tag=None):
+        super(XMLList, self).__init__(list)
         self.xml_tag = xml_tag
