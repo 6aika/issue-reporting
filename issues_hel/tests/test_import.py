0858d7ded502ee7d1a31d8df767bf1b52648e32e
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-05-31 21:38:22 +0300

    Fix failing import test (how did this ever work?)

--- issues_hel/tests/test_import.py
+++ issues_hel/tests/test_import.py
@@ -3,13 +3,12 @@ import os
 
 import pytest
 
-from issues.models import Issue
 from issues.sync.down import update_local_issue
 
 
 @pytest.mark.django_db
 def test_import_taskful_georeport():
-    with open(os.path.join(os.path.dirname(__file__), "taskful_request.json"), "r") as infp:
+    with open(os.path.join(os.path.dirname(__file__), "taskful_request.json"), "r", encoding="utf8") as infp:
         data = json.load(infp)
     issue, created = update_local_issue(data, 'import')
     assert created
