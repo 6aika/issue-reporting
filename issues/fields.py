d8e8188987ea0b7cab69d13ecf768b2446f44600
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-05-31 21:42:37 +0300

    Add FallbackPointField.
    This is a special field that is a GIS-enabled pointfield iff:
    
    * the `default` database backend is GIS enabled
    
    * GIS features are importable
    
    As this is a masquerading subclass, migrations should not be affected, however switching an onstallation from a gissy backend to a gisless backend is _not_ supported.
    
    Refs #5

---
+++ issues/fields.py
@@ -0,0 +1,63 @@
+import re
+
+from django.db.models import CharField
+from six import string_types
+
+from issues.gis import determine_gissiness
+
+try:
+    from django.contrib.gis.geos import Point
+except:
+    Point = None
+
+GEOS_POINT_EXPRESSION_RE = re.compile(r'.*POINT \((?P<lon>.+?) (?P<lat>.+?)\)')
+
+
+def _string_value_to_coords(value):
+    geos_match = GEOS_POINT_EXPRESSION_RE.match(value)
+    if geos_match:
+        value = "%(lon)s;%(lat)s" % geos_match.groupdict()
+    latlng = tuple(float(c) for c in value.split(";"))
+    assert len(latlng) == 2
+    assert all(-180 < c < 180 for c in latlng)
+    return latlng
+
+
+class _FallbackPointField(CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("max_length", 128)
+        kwargs['db_index'] = False
+        kwargs.pop('srid', False)
+        super().__init__(*args, **kwargs)
+
+    def from_db_value(self, value, expression, connection, context):
+        if not value:
+            return None
+        latlng = _string_value_to_coords(value)
+        return latlng
+
+    def to_python(self, value):
+        if (Point and isinstance(value, Point)) or isinstance(value, (list, tuple)):
+            value = "%s;%s" % (value[0], value[1])
+
+        return self.from_db_value(value, None, None, None)
+
+    def get_prep_value(self, value):
+        if not value:
+            return None
+        if isinstance(value, string_types):
+            lat, lng = _string_value_to_coords(value)
+        else:
+            lat, lng = [float(c) for c in value]
+        return "%r;%r" % (lat, lng)
+
+
+if determine_gissiness():
+    from django.contrib.gis.db.models.fields import PointField as _GISPointField
+
+
+    class FallbackPointField(_GISPointField):
+        pass
+else:
+    class FallbackPointField(_FallbackPointField):
+        pass
---
+++ issues/tests/test_gis.py
@@ -0,0 +1,21 @@
+import pytest
+from django.conf import settings
+
+from issues.gis import determine_gissiness
+
+GISSY = {
+    'django.contrib.gis.db.backends.postgis': True,
+    'django.db.backends.mysql': False,
+    'django.db.backends.sqlite3': False,
+}
+
+
+def test_gissiness():
+    """
+    Test that determine_gissiness() correctly determines gissiness for known databases.
+    :return:
+    """
+    engine = settings.DATABASES['default']['ENGINE']
+    if engine not in GISSY:
+        pytest.skip('I have no idea whether %s should be gissy' % engine)
+    assert GISSY[engine] == determine_gissiness()
