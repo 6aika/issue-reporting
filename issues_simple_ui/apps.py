0f20ce37d2a5c9822510c90465eacfd36b615f41
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-06-07 17:35:54 +0300

    Simple UI: Require issue log extension

--- issues_simple_ui/apps.py
+++ issues_simple_ui/apps.py
@@ -1,6 +1,13 @@
-from django.apps import AppConfig
+from django.apps import AppConfig, apps
+from django.core.exceptions import ImproperlyConfigured
 
 
 class IssuesSimpleUiConfig(AppConfig):
     name = 'issues_simple_ui'
     verbose_name = 'Simple Issue UI'
+
+    def ready(self):
+        try:
+            apps.get_model("issues_log.LogEntry")
+        except LookupError:
+            raise ImproperlyConfigured("Using the Simple UI requires the log extension.")
