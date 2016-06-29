c2c218a7cab2b9a057953d626fe4574104d75909
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-06-29 09:45:43 +0300

    Drop lxml dependency

--- issues/api/transforms.py
+++ issues/api/transforms.py
@@ -1,7 +1,7 @@
 import json
 from collections import OrderedDict
 
-from lxml import etree
+from xml.etree import ElementTree as ET
 from six import text_type
 
 
@@ -9,13 +9,13 @@ def spark_node(node, in_list=False):
     """
     "Sparkify" a single XML node.
 
-    :param node: The LXML node to sparkify
-    :type node: lxml.etree.Element
+    :param node: The XML node to sparkify
+    :type node: ET.Element
     :param in_list: Internal flag; used by this fn when sparkifying a list
     :type in_list: bool
     :return: list|dict
     """
-    kids = node.getchildren()
+    kids = list(node)
     if kids and all(c.tag == kids[0].tag for c in kids):  # Homogeneous children; this must be an array
         children = [
             node for node in [spark_node(k, in_list=True) for k in kids]
@@ -26,9 +26,11 @@ def spark_node(node, in_list=False):
         if not children:
             return {}
 
-        if all(isinstance(j_kid, text_type) for j_kid in children):  # All of the children turned into bare strings:
+        if all(isinstance(j_kid, text_type) for j_kid in children):
+            # All of the children turned into bare strings:
             return {node.tag: children}  # Wrap them in an object
-        elif all(isinstance(j_kid, dict) and len(j_kid) == 1 for j_kid in children):  # The children got turned into 1-dicts?
+        elif all(isinstance(j_kid, dict) and len(j_kid) == 1 for j_kid in children):
+            # The children got turned into 1-dicts?
             n_kids = {}
             for kid in children:
                 n_kids.update(kid)
@@ -62,6 +64,6 @@ def transform_xml_to_json(xml):
     """
     if isinstance(xml, text_type):
         xml = xml.encode("UTF-8")  # Here's hoping it's UTF-8
-    xml = etree.fromstring(xml)
+    xml = ET.fromstring(xml)
     dct = spark_node(xml)
     return json.dumps(dct)
